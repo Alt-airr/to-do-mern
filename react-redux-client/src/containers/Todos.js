0414799544ed20db87c9bd2e4db10419556620a2
Alt-airr <52180775+Alt-airr@users.noreply.github.com>
2020-02-18 19:24:29 +0200

    Add files via upload

---
+++ react-redux-client/src/containers/Todos.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import * as todoActions from '../actions/todoActions';
+import Todos from '../components/Todos';
+
+const mapStateToProps = (state,ownProps) => {
+  return {
+    mappedTodoState: state.todoState
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchTodos: () => dispatch(todoActions.fetchTodos()),
+    mappedEditTodo: todoToEdit => dispatch(todoActions.editTodo(todoToEdit)),
+    mappedshowEditModal: todoToEdit => dispatch(todoActions.showEditModal(todoToEdit)),
+    mappedhideEditModal: () => dispatch(todoActions.hideEditModal()),
+    mappedDeleteTodo: todoToDelete => dispatch(todoActions.deleteTodo(todoToDelete)),
+    mappedshowDeleteModal: todoToDelete => dispatch(todoActions.showDeleteModal(todoToDelete)),
+    mappedhideDeleteModal: () => dispatch(todoActions.hideDeleteModal())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Todos);
